ef8190118e82fe5024b55149fe343db46a736c36
Rumaysa babulkhair <111044066+rumaysaa@users.noreply.github.com>
2024-02-15 17:52:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+app.route('/',methods=['get'])
+def get():
+  return request.remote_addr
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000,debug=True)
