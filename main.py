75b601a1c4c6a1d89e585e496678e8ea18cde399
Rumaysa babulkhair <111044066+rumaysaa@users.noreply.github.com>
2024-02-15 18:11:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/',methods=['get'])
 def get():
-  return request.remote_addr
+    if 'X-Forwarded-For' in request.headers:
+# The header can contain a comma-separated list of IPs, the first one being the client's IP
+client_ips = request.headers['X-Forwarded-For'].split(',')
+        client_ip = client_ips[0].strip()
+    else:
+        client_ip = request.remote_addr
+    return f"Your real IP address is: {client_ip}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000,debug=True)
